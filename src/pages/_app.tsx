f2d403d0e4e8ec6bda60ca7c5c0e44a1056831a2
Lucas Borges <lscborges1@gmail.com>
2021-04-22 13:45:38 -0300

    finished player + added json server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import '../styles/global.scss'
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 import styles from '../styles/app.module.scss'
-import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
